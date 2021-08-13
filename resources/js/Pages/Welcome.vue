1a387262eebd15146cdbb253bdd52f28a7eb6811
ayoubbajjou <bajjouayoub@gmail.com>
2021-08-13 11:26:52 +0100

    working on navbar componenet

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <Navbar />
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+import Navbar from '../Components/Navbar'
+
+    export default {
+        components: {
+            Navbar
+        },
+
+        props: {}
+    }
+</script>
