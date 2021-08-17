83841fad27c4051ee6676e39970f84cdb561c4d0
ayoubbajjou <bajjouayoub@gmail.com>
2021-08-18 00:36:15 +0100

    fix: refactor the previous changes in the header and top navbar

--- resources/js/Components/Header/LogoHeader.vue
+++ resources/js/Components/Header/LogoHeader.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="w-1/4">
-        <img src="/images/aliexpress-logo.png" alt="">
+        <LogoImage />
         <span class="text-xs text-gray-500">Smart Shopping, Better Living!</span>
     </div>
 </template>
+
+<script>
+import LogoImage from '../images/LogoImage.vue'
+export default {
+    components: {
+        LogoImage
+    }
+}
+</script>
--- resources/js/Components/Header/TopNavbar.vue
+++ resources/js/Components/Header/TopNavbar.vue
@@ -10,17 +10,13 @@
                 </li>
                 <li>
                     <a href="/" class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
+                        <WishlistIcon class="h-5 w-5 mr-1" />
                         Wishlist
                     </a>
                 </li>
                 <li @mouseover="isAccountLinkHovered = true" class="relative cursor-pointer">
                     <span :class="isAccountLinkHovered ? 'text-red-400' : ''" class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                        </svg>
+                        <UserIcon class="h-5 w-5 mr-1" />
                         Account
                     </span>
                     <div v-if="isAccountLinkHovered" @mouseleave="isAccountLinkHovered = false" class="absolute right-0 w-max h-max p-4 bg-white border-2 border-gray-300 mt-3 z-50">
@@ -51,7 +47,13 @@
 </template>
 
 <script>
+import UserIcon from '../Icons/UserIcon.vue'
+import WishlistIcon from '../Icons/WishlistIcon.vue'
 export default {
+    components: {
+        UserIcon,
+        WishlistIcon
+    },
     data() {
         return {
             isAccountLinkHovered: false
