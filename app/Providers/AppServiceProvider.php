09fb3a7184a0a2b50545b34535fda5ec8a740582
Neo <enginesilver@gmail.com>
2021-08-18 20:43:09 +0200

    Adding helpers model and query builder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Builder as DBBuilder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::preventLazyLoading(! app()->isProduction());  // prevent N+1  on develop
+        Schema::defaultStringLength(191);
+        Builder::defaultStringLength(191);
+
+        // macro for queries like  Model::whereLike('name', 'Joe')
+
+        #WHERE
+        DBBuilder::macro('whereLike', function ($attribute, string $searchTerm) {
+            return $this->where($attribute, 'LIKE', "%{$searchTerm}%");
+        });
+
+        DBBuilder::macro('whereStartLike', function ($attribute, string $searchTerm) {
+            return $this->where($attribute, 'LIKE', "{$searchTerm}%");
+        });
+
+        DBBuilder::macro('whereEndLike', function ($attribute, string $searchTerm) {
+            return $this->where($attribute, 'LIKE', "%{$searchTerm}");
+        });
+
+        #OR WHERE
+
+        DBBuilder::macro('orWhereLike', function ($attribute, string $searchTerm) {
+            return $this->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+        });
+
+        DBBuilder::macro('orWhereStartLike', function ($attribute, string $searchTerm) {
+            return $this->orWhere($attribute, 'LIKE', "{$searchTerm}%");
+        });
+
+        DBBuilder::macro('orWhereEndLike', function ($attribute, string $searchTerm) {
+            return $this->orWhere($attribute, 'LIKE', "%{$searchTerm}");
+        });
     }
 }
